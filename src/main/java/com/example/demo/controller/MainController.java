d51cf6990a691c66c9e24b1436006d327208f9fd
ty <qksrm5546@gmail.com>
2024-06-03 17:58:09 +0900

    ex

---
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class MainController {
+
+    @GetMapping(value = {"/index", "/"})
+    public String index() {
+        return "/index";
+    }
+
+}
